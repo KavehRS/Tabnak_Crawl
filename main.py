97f398f5babe86a62af4706eb7f38b7e64ffea63
Kaveh Rezaei <kavehrs@pm.me>
2023-01-23 11:21:26 +0330

    Update main.py

--- main.py
+++ main.py
@@ -69,51 +69,91 @@ for i in range(0, 48):
         t2l = 'http://www.tabnak.ir' + Content.get('href')
         Tabnak2.loc[i, "NewsLink"] = t2l
         Tabnak2.loc[i, "NewsTitr"] = Content.get('title')
-        Tcb = soup_link.findAll('div', {'class': "lead1"})[i]
+        Tcb = soup_link.findAll('div', {'class': "lead1"})
         Tabnak2.loc[i, "NewsTitrBrief"] = Tcb.text
         bb = requests.get(t2l)[i]
-        tcl = BeautifulSoup(bb.text, 'html.parser')[i]
+        contentB = BeautifulSoup(bb.text, 'html.parser')[i]
 # count paragraphs
-        counterB = 0
-        for k in tcl.findAll('p'):
+        countB = 0
+        for k in contentB.findAll('p'):
             if k:
-                counterB = counterB + 1
+                countB = countB + 1
+        fullcontentB = ''
 # get paragraphs and put to DataFrame
-        fullnewsB = ''
-        for n in range(counterB):
-            af = tcl.findAll('p')[n]
-            fullnewsB = fullnewsB + '\n' + af.text
-        Tabnak2.loc[i, "NewsText"] = fullnewsB
+        for l in range(countB):
+            paragraphB = contentB.find('p')[l]
+            fullcontentB = fullcontentB + '\n' + paragraphB.text
+            Tabnak2.loc[i, "NewsText"] = fullcontentB
 
-        # Tabnak2.loc[i, "NewsGroup"] = 0
-        # Tabnak2.loc[i, "NewsGroupSub"] = 0
-        # Tabnak2.loc[i, "KeyWord"] = 0
-        # Tabnak2.loc[i, "ReleaseTimeNews"] = 0
-        # Tabnak2.loc[i, "ReleaseDateNews"] = 0
+        pageB_link = BeautifulSoup(t2l.text, 'html.parser')[i]
+    # GET NewsGroup
+        NewsGroupB = pageB_link.find('div', {'class': "news_path"}).text
+        sepA = NewsGroupB.split('»')
+        Tabnak2.loc[i, "NewsGroup"] =sepA[0]
+        Tabnak2.loc[0, "NewsGroupSub"] = sepA[1]
+# count tags
+        tagB_count = 0
+        for m in pageB_link.findAll('a', {'class': "btn btn-primary-news"}):
+            if m:
+                tagB_count = tagB_count + 1
 
-
-
+# get Tags and put to DataFrame
+        AllTagBs = ''
+        for n in range(tagB_count):
+            tagB = pageB_link.findAll('a', {'class': "btn btn-primary-news"})[n]
+            AllTagBs = AllTagBs + '#' + tagB.text
+        Tabnak2.loc[i, "KeyWord"] = AllTagBs
+# get Date & Time
+        content_time = pageB_link.find('div', {'class': "news_nav news_pdate_c"}).text
+        faB_time = content_time.split('-')
+        Tabnak2.loc[i, "ReleaseDateNews"] = faB_time[0].split(':')[1]
+        Tabnak2.loc[i, "ReleaseTimeNews"] = faB_time[1].split()[0]
 
     except:
-             pass
-
-
-
-
-
-
-
-
-
-
-
-
+        pass
+##############################################################################################
 # last News
 Lnl = soup_link.find('div', {'id': "tab1_aa"})
 for i in range(1, 200):
      try:
         Lastnews = Lnl.findAll('a')[i]
-        Tabnak3.loc[i, "NewsLink"] = Lastnews.get('href')
+        lastnews_link = 'http://www.tabnak.ir' + Lastnews.get('href')
+        Tabnak3.loc[i, "NewsLink"] = lastnews_link
         Tabnak3.loc[i, "NewsTitr"] = Lastnews.get('title')
-      except:
+        lastnews_content = BeautifulSoup(lastnews_link.text, 'html.parser')
+        lastnewBrief = lastnews_content.findAll('div', {'class': "subtitle"})
+        Tabnak3.loc[i, "NewsTitrBrief"] = lastnewBrief.text
+     # count paragraphs
+        countC = 0
+        for p in lastnews_content.findAll('p'):
+            if p:
+                countC = countC + 1
+        fullLastnews = ''
+     # get paragraphs and put to DataFrame
+        for q in range(countC):
+            paragraphL  = lastnews_content.find('p')[q]
+            fullLastnews = fullLastnews + '\n' + paragraphL.text
+            Tabnak3.loc[i, "NewsText"] = fullLastnews
+     # GET NewsGroup
+        NewsGroupL = lastnews_content.find('div', {'class': "news_path"}).text
+        sepL = NewsGroupL.split('»')
+        Tabnak3.loc[i, "NewsGroup"] = sepL[0]
+        Tabnak3.loc[0, "NewsGroupSub"] = sepL[1]
+     # count tags
+        tagL_count = 0
+        for u in lastnews_content.findAll('a', {'class': "btn btn-primary-news"}):
+            if u:
+                tagL_count = tagL_count + 1
+    # get Tags and put to DataFrame
+        AllTagL = ''
+        for w in range(tagL_count):
+            tagL = lastnews_content.findAll('a', {'class': "btn btn-primary-news"})[w]
+            AllTagBs = AllTagL + '#' + tagL.text
+        Tabnak3.loc[i, "KeyWord"] = AllTagL
+     # get Date & Time
+        contentL_time = lastnews_content.find('div', {'class': "news_nav news_pdate_c"}).text
+        faL_time = contentL_time.split('-')
+        Tabnak3.loc[i, "ReleaseDateNews"] = faL_time[0].split(':')[1]
+        Tabnak3.loc[i, "ReleaseTimeNews"] = faL_time[1].split()[0]
+     except:
                pass
